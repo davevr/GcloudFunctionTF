aa26e985d64b9fb39e1e70543b01c6d0a27a60d1
Dave Vronay <davevr@gmail.com>
2021-04-18 12:24:58 -0700

    basic TF working

--- tf/main.tf
+++ tf/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "google" {
 
-  credentials = file("whattheflockserver-de8a8db8d2f0.json")
+  # credentials = file("whattheflockserver-de8a8db8d2f0.json")
 
   project = "whattheflockserver"
   region  = "us-central1"
@@ -17,26 +17,38 @@ provider "google" {
 }
 
 resource "google_storage_bucket" "bucket" {
-  name = "cloud-function-tutorial-bucket" # This bucket name must be unique
+  name = "wtfserver-cloud-function-test-bucket" # This bucket name must be unique
 }
 
+data "archive_file" "src" {
+  type        = "zip"
+  source_dir  = "${path.root}/../src" # Directory where your Python source code is
+  output_path = "${path.root}/../generated/src.zip"
+}
 
 resource "google_storage_bucket_object" "archive" {
   name   = "${data.archive_file.src.output_md5}.zip"
   bucket = google_storage_bucket.bucket.name
+  source = "${path.root}/../generated/src.zip"
 }
-
 
 resource "google_cloudfunctions_function" "wtffunctiontest" {
   name        = "tffunctiontest"
   description = "My function"
   runtime     = "nodejs12"
-   source_archive_bucket = google_storage_bucket.bucket.name
+  source_archive_bucket = google_storage_bucket.bucket.name
   source_archive_object = google_storage_bucket_object.archive.name
 
   available_memory_mb   = 128
   trigger_http          = true
   entry_point           = "handler"
+   environment_variables = {
+    IS_IN_GCLOUD = "TRUE"
+    MYSQL_DB = "wtfdata"
+    MYSQL_PASSWORD = "secret"
+    MYSQL_USER = "root"
+    CLOUD_SQL_CONNECTION_NAME = "whattheflockserver:us-west1:wtf-server-database"
+  }
 }
 
 resource "google_cloudfunctions_function_iam_member" "invoker" {
