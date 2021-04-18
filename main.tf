1009521e3ee7d6a7ec83382f13c98e237f9b95ec
Dave Vronay <davevr@gmail.com>
2021-04-18 11:37:36 -0700

    initial update

--- main.tf
+++ main.tf
@@ -1,25 +1,38 @@
 terraform {
   required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
     }
   }
 }
 
-provider "docker" {
-  host = "npipe:////.//pipe//docker_engine"
+provider "google" {
+
+  credentials = file("whattheflockserver-de8a8db8d2f0.json")
+
+  project = "whattheflockserver"
+  region  = "us-central1"
+  zone    = "us-central1-c"
 }
 
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+
+resource "google_cloudfunctions_function" "function" {
+  name        = "function-test"
+  description = "My function"
+  runtime     = "nodejs12"
+
+  available_memory_mb   = 128
+  trigger_http          = true
+  entry_point           = "handler"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
+# IAM entry for all users to invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
 }
